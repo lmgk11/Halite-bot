45b51756826b0aa92834b4ad071f6f0bf01c4aeb
lmgk11 <lulle.gillbergkling@gmail.com>
2018-06-16 01:08:20 +0200

    bot

--- MyBot-v2.py
+++ MyBot-v2.py
@@ -6,5 +6,28 @@ game = hlt.Game("Milo")
 logging.info("Starting Milo")
 
 while True:
+    my_ships = game_map.get_me().all_ships()
+
     game_map = game.update_map()
     command_queue = []
+
+    for ship in my_ships:
+        ship_id = ship.id
+        if ship.docking_status != ship.DockingStatus.UNDOCKED:
+            continue
+
+        entities_by_distance = game_map.nearby_entities_by_distance(ship)
+        entities_by_distance = OrderedDict(sorted(entities_by_distance.items(), key=lambda t: t[0]))
+        closest_empty_planet = [entities_by_distance[distance][0] for distance in entities_by_distance if isinstance(entities_by_distance[distance][0], hlt.entity.Planet) and not entities_by_distance[distance][0].is_owned()]
+
+        if len(closest_empty_planets) > 0 and ship.can_dock(closest_empty_planets[0]):
+            command_queue.append(ship.dock(closest_empty_planet[0]))
+        else:
+            navigate_command = ship.navigate(
+                ship.closest_point_to(planet),
+                game_map,
+                speed=int(hlt.constants.MAX_SPEED),
+                ignore_ships=True)
+        if navigate_command:
+            command_queue.append(navigate_command)
+    game.send_command_queue(command_queue)
